46509ed7f3009014487c5f940ae318e425552548
MaropenSamuel <rabopapesamuel@gmail.com>
2024-06-01 20:54:04 +0200

    adding pokemon imgs

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,29 @@ async function getPokemnoByName(name: string): Promise<void> {
   const dataResponse =await fetch(url);
   const pokemonData = await dataResponse.json();
 
+  console.log(pokemonData.name);
+  console.log(pokemonData.height);
+  console.log(pokemonData.weight);
+  console.log(pokemonData);
+
 }
 
+// function getPokemnoByName(name: string) {
+//   const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+//   let pokemonData = {}
+
+//   fetch(url)
+//     .then((data: any) => {
+
+//       const dataResponse = data?.json();
+
+//       dataResponse.then((pokemon:any)=>{
+        
+//         pokemonData = pokemon;
+
+//       });
+//     })
+// console.log(pokemonData)
+
+// }
 getPokemnoByName("pikachu");
