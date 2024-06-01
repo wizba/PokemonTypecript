089d3c53ff58dc7db8390029a628d266e587d848
william <williamrabs@gmail.com>
2024-06-01 12:58:21 +0200

    Added an endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hello World")
+function getPokemnoByName(name: string): void {
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+  console.log(url);
+  fetch(url).then(async(response)=>{
+    const data:any =await response.json();
+    console.log("my response ", data);
+  })
+}
+
+getPokemnoByName("pikachu");
