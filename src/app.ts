e8c71603955f6705dfbd8d779c1f0e3e5019fff9
MaropenSamuel <rabopapesamuel@gmail.com>
2024-06-02 15:09:09 +0200

    adding details to pokemon

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,23 @@ async function getPokemnoByName(name: string): Promise<void> {
   console.log(pokemonData.weight);
   console.log(pokemonData);
 
+  const imgContainer = document.getElementById("img-holder"); // get container element that is gonna hold the image
+  const imgTag = document.createElement('img'); // create an image element
+
+  imgTag.src=pokemonData.sprites.other.dream_world.front_default; // add an image from api to src of the new image element
+  imgTag.classList.add("house-img"); // add a class to the image 
+
+  imgContainer?.appendChild(imgTag) // add the image element to the image conatainer
+
+  console.log('imgTag ',imgTag);
+
+const nameContainer =document.getElementById("details");
+  const nameParagraph = document.createElement('p');
+  
+nameParagraph.textContent = pokemonData.name;
+nameContainer?.appendChild(nameParagraph);
+
+
 }
 
 // function getPokemnoByName(name: string) {
@@ -29,4 +46,4 @@ async function getPokemnoByName(name: string): Promise<void> {
 // console.log(pokemonData)
 
 // }
-getPokemnoByName("pikachu");
+getPokemnoByName("abra");
