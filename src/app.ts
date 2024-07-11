1ac1fe74451d1a8d771ccf8350aea801dc581e20
MaropenSamuel <rabopapesamuel@gmail.com>
2024-07-11 14:29:46 +0200

    git

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ async function getPokemnoByName(name: string): Promise<void> {
 
   const imgContainer = document.getElementById("img-holder"); // get container element that is gonna hold the image
   const imgTag = document.createElement('img'); // create an image element
-
+  
   imgTag.src=pokemonData.sprites.other.dream_world.front_default; // add an image from api to src of the new image element
   imgTag.classList.add("house-img"); // add a class to the image 
 
@@ -22,28 +22,23 @@ async function getPokemnoByName(name: string): Promise<void> {
 const nameContainer =document.getElementById("details");
   const nameParagraph = document.createElement('p');
   
-nameParagraph.textContent = pokemonData.name;
+nameParagraph.textContent = `Name: ${pokemonData.name}`;
 nameContainer?.appendChild(nameParagraph);
+console.log('name',nameContainer);
 
+const weightContainer = document.getElementById("weight");
+    const weightParagraph = document.createElement('p');
 
+    weightParagraph.textContent = `Weight: ${pokemonData.weight}`;
+    weightContainer?.appendChild(weightParagraph);
+    console.log('weight', weightContainer);
+
+    const heightContainer = document.getElementById("height");
+    const heightParagraph = document.createElement('p');
+
+    heightParagraph.textContent = `Height: ${pokemonData.height}`;
+    heightContainer?.appendChild(heightParagraph);
+    console.log('height', heightContainer);
 }
 
-// function getPokemnoByName(name: string) {
-//   const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
-//   let pokemonData = {}
-
-//   fetch(url)
-//     .then((data: any) => {
-
-//       const dataResponse = data?.json();
-
-//       dataResponse.then((pokemon:any)=>{
-        
-//         pokemonData = pokemon;
-
-//       });
-//     })
-// console.log(pokemonData)
-
-// }
 getPokemnoByName("abra");
